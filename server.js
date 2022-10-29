2721fe905f500c02625025fe8285b25534e41da0
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-10-29 18:44:08 -0400

    Implement modules - export+import

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
-const { unlink } = require('fs');
+const addNums = require('./addNums');
 
-unlink('hello.txt', (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('File deleted!');
-});
+const sum = addNums(2, 2);
+console.log(sum);
